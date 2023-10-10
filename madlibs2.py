8780a1f6806de5a4181ce11ae4d48bea19065d95
AdityaM0han <144744325+AdityaM0han@users.noreply.github.com>
2023-10-10 18:44:45 -0400

    Add files via upload

---
+++ madlibs2.py
@@ -0,0 +1,25 @@
+filename = input("Type your name: ") +".txt"
+with open(filename) as f:
+    a1 = f.readline().strip()
+    a2 = f.readline().strip()
+    a3 = f.readline().strip()
+    a4 = f.readline().strip()
+    a5 = f.readline().strip()
+    a6 = f.readline().strip()
+    a7 = f.readline().strip()
+    a8 = f.readline().strip()
+    a9 = f.readline().strip()
+    a10 = f.readline().strip()
+    a11 = f.readline().strip()
+    a12 = f.readline().strip()
+    a13 = f.readline().strip()
+    a14 = f.readline().strip()
+
+
+    print("There was once a " + a1+  " named " + a2 + ". One day, when " + a2 + " was " + a3 + " he saw a " + a4 + " " + a5 + ". " + a2 + " was so " + a6 + " to see a " + a5 + "!")
+    print("Suddenly, the " + a5 + " started "  +  a7 + "! It was the " + a8 + " thing that had ever happened to " + a2)
+    print("After " + a2 + " " + a14 + " away from the " + a5 + ", he went to " + a9 + ". " + a2 + " got his favorite meal: a " + a10 + "!")
+    print("Finally, " + a2 + " went to his house on " + a11 + " street. As " + a2 +" closed his " + a12 + " and went to bed, " + a2 + " thought today was the most " + a13 + " day ever.") 
+
+ 
+ 
